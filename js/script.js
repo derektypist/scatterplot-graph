79854ed5b2c3a63090c094a7801ce55581a52b8a
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-10-05 08:43:27 +0000

    Added variables in script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Initialise Variables
+
+let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+let req = new XMLHttpRequest();
+let heightScale;
+let xScale;
+let xAxisScale;
+let yAxisScale;
+let width = 850;
+let height = 650;
+let padding = 40;
