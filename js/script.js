66b03055953eda68a3da68ea886e9603f3d7ca89
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-10-06 14:14:37 +0000

    Updated generateScales function at script.js

--- js/script.js
+++ js/script.js
@@ -21,6 +21,16 @@ let drawCanvas = () => {
 
 // Set Up Scales
 let generateScales = () => {
-    xScale = d3.scaleLinear().range([0, width]);
-    heightScale = d3.scaleTime().range([0, height]);
+    xScale = d3.scaleLinear()
+        .domain([d3.min(values, (item) => {
+            return item['Year']}) -1, d3.max(values, (item) => {
+                return item['Year']}) + 1])
+        .range([padding, width-padding]);
+            
+    yScale = d3.scaleTime()
+        .domain([d3.min(values, (item) => {
+            return new Date(item['Seconds'] * 1000)}), d3.max(values, (item) => {
+                return new Date(item['Seconds'] * 1000)
+        })])
+        .range([padding, height-padding]);
 };
